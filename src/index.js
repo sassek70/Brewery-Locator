ad206026521c4a4cf55bab913a1c1e3c7e6966ce
Kevin Sasse <sassek70@gmail.com>
2022-10-05 15:23:54 -0400

    added search and notes features

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const baseUrl = `https://api.openbrewerydb.org/breweries`
 const randomUrl = `https://api.openbrewerydb.org/breweries/random`
 let localUrl = `http://localhost:3000/breweries`
+let localVisitedUrl = `http://localhost:3000/breweries?visited=true`
+let localFavoriteUrl = `http://localhost:3000/breweries?favorite=true`
 let breweryObj 
 const mainContainer = document.getElementById('display-area')
-
 
 // 163 pages, 8,106 breweries in database
 
@@ -53,101 +54,119 @@ favorites.addEventListener('click', (e) => {
 const visited = document.getElementById('visited')
 visited.addEventListener('click', (e) => {
     resetDisplayAll()
-    showFavoritesList()
+    showVisitedList()
 })
 
 //Display full brewery details
 const displayBreweryDetails = (brewery) => {
-        //Clear DOM container
-        resetDisplay()
+    
+    //Clear DOM container
+    resetDisplay()
+    
+    //Create all elements when invoked
+    const div = document.createElement('div')
+    const title = document.createElement('h2')
+    const address = document.createElement('p')
+    const country = document.createElement('p')
+    const phone = document.createElement('p')
+    const website = document.createElement('p')
+    const type = document.createElement('p')        
+    // const visitedStatus = document.createElement('p')
+    const visitedButton = document.createElement('button')
+    // const favoriteStatus = document.createElement('p')
+    const favoriteButton = document.createElement('button')
+    const userNotes = document.createElement('p')
+    const addNotesForm = document.createElement('form')
+    const newNotes = document.createElement('input')
+    const newNotesButton = document.createElement('button')
+    
+    
+    //update element data to match currently displayed brewery
+    div.id = `detail-display`
+    visitedButton.id = `visited-button`
+    favoriteButton.id = `favorite-button`
+    userNotes.id = `notes`
+    title.textContent = brewery.name
+    address.textContent = `Address: ${brewery.street || ``}, ${brewery.city || ``}, ${brewery.state || ``} ${brewery.postal_code || ``}`
+    country.textContent = `Country of Origin: ${brewery.country}`
+    phone.textContent = `Phone: ${phoneNumberFormat(brewery)}`      
+    website.textContent = `Website: ${brewery.website_url || 'Not Available'}`
+    type.textContent = `Brewery Type: ${brewery.brewery_type || 'Not Available'}`
+    visitedButton.textContent = checkVisitedStatus(brewery)
+    favoriteButton.textContent = checkFavoriteStatus(brewery)
+    newNotesButton.textContent = `Add Note`
+    userNotes.textContent = brewery.notes
+    
+    //append all to DOM container      
+    mainContainer.appendChild(div)
+    div.appendChild(title)
+    div.appendChild(address)
+    div.appendChild(country)
+    div.appendChild(phone)
+    div.appendChild(website)
+    div.appendChild(type)
+    div.appendChild(addNotesForm)
+    addNotesForm.appendChild(newNotes)
+    addNotesForm.appendChild(newNotesButton)
+    // div.appendChild(visitedStatus)
+    div.appendChild(visitedButton)
+    // div.appendChild(favoriteStatus)
+    div.appendChild(favoriteButton)
+    div.appendChild(userNotes)
+          
 
-        //Create all elements when invoked
-        // const mainContainer = document.getElementById('display-area')
-        const div = document.createElement('div')
-        const title = document.createElement('h2')
-        const address = document.createElement('p')
-        const country = document.createElement('p')
-        const phone = document.createElement('p')
-        const website = document.createElement('p')
-        const type = document.createElement('p')        
-        // const visitedStatus = document.createElement('p')
-        const visitedButton = document.createElement('button')
-        // const favoriteStatus = document.createElement('p')
-        const favoriteButton = document.createElement('button')
-        const userNotes = document.createElement('p')
-        const addNotes = document.createElement('form')
-        const newNotes = document.createElement('input')
-        const newNotesButton = document.createElement('button')
+    // fetch(localFavoriteUrl)
+    // .then(res => res.json())
+    // .then(localDB => localDB.forEach(brewery => console.log(brewery.id)))
 
-        //remove unnecessary information from the API Obj
-        breweryObj = filterBreweryObjKeys(brewery)
-        
-        //update element data to match currently displayed brewery
-        div.id = `detail-display`
-        visitedButton.id = `visited-button`
-        favoriteButton.id = `favorite-button`
-        userNotes.id = `notes`
-        title.textContent = breweryObj.name
-        address.textContent = `Address: ${breweryObj.street || ``}, ${breweryObj.city || ``}, ${breweryObj.state || ``} ${breweryObj.postal_code || ``}`
-        country.textContent = `Country of Origin: ${breweryObj.country}`
-        phone.textContent = `Phone: ${phoneNumberFormat(breweryObj)}`      
-        website.textContent = `Website: ${breweryObj.website_url || 'Not Available'}`
-        type.textContent = `Brewery Type: ${breweryObj.brewery_type || 'Not Available'}`
-        visitedButton.textContent = `Add to Visited Breweries`
-        favoriteButton.textContent = `Add to Favorites`
-        newNotesButton.textContent = `Add Note`
-        userNotes.textContent = breweryObj.notes
+    //remove unnecessary information from the API Obj
+    breweryObj = filterBreweryObjKeys(brewery)
 
-        //append all to DOM container      
-        mainContainer.appendChild(div)
-        div.appendChild(title)
-        div.appendChild(address)
-        div.appendChild(country)
-        div.appendChild(phone)
-        div.appendChild(website)
-        div.appendChild(type)
-        div.appendChild(addNotes)
-        addNotes.appendChild(newNotes)
-        addNotes.appendChild(newNotesButton)
-        // div.appendChild(visitedStatus)
-        div.appendChild(visitedButton)
-        // div.appendChild(favoriteStatus)
-        div.appendChild(favoriteButton)
-        div.appendChild(userNotes)
-              
+    //Button to toggle Visited status and add to Visited list
+    visitedButton.addEventListener('click', (e) => {
+        console.log(breweryObj)
+        if(visitedButton.textContent == `Add to Visited Breweries`){
+            visitedButton.textContent = `Remove from Visited Breweries`
+            breweryObj.visited = true
+            addBreweryToLocal(localUrl, breweryObj)
+            console.log(breweryObj)
+        } else if (visitedButton.textContent == `Remove from Visited Breweries`) {
+            breweryObj.visited = false
+            visitedButton.textContent = `Add to Visited Breweries`
+            console.log(breweryObj)
+        }
+    })
+   
+    //Button to toggle Favorites status and add to Favorites list
+    favoriteButton.addEventListener('click', (e) => {
+        if(favoriteButton.textContent == `Add to Favorites`){
+            breweryObj.favorite = true
+            breweryObj.visited = true
+            favoriteButton.textContent = `Remove from Favorites`
+            addBreweryToLocal(localUrl, breweryObj)
+        } else if (favoriteButton.textContent == `Remove from Favorites`) {
+            breweryObj.favorite = false
+            favoriteButton.textContent = `Add to Favorites`
+           
+        }
+        console.log(breweryObj.id)
+    })
 
-        //Button to toggle Visited status and add to Visited list
-        visitedButton.addEventListener('click', (e) => {
-            if(visitedButton.textContent == `Add to Visited Breweries`){
-                breweryObj.visited = true
-                visitedButton.textContent = `Remove from Visited Breweries`
-                addBreweryToLocal(localUrl, breweryObj)
-                console.log(breweryObj)
-            } else if (visitedButton.textContent == `Remove from Visited Breweries`) {
-                breweryObj.visited = false
-                visitedButton.textContent = `Add to Visited Breweries`
-                console.log(breweryObj)
-            }
+        newNotesButton.addEventListener('click', (e) => {
+            e.preventDefault()
+            breweryObj.notes.push(newNotes.value)
+            updateLocalBrewery(`http://localhost:3000/breweries/${breweryObj.id}`, {notes: newNotes.value})
+            console.log(breweryObj)
+            addNotesForm.reset()
+
+
         })
 
-        //Button to toggle Favorites status and add to Favorites list
-        favoriteButton.addEventListener('click', (e) => {
-            if(favoriteButton.textContent == `Add to Favorites`){
-                breweryObj.favorite = true
-                breweryObj.visited = true
-                favoriteButton.textContent = `Remove from Favorites`
-                addBreweryToLocal(localUrl, breweryObj)
-                console.log(breweryObj)
-            } else if (favoriteButton.textContent == `Remove from Favorites`) {
-                breweryObj.favorite = false
-                favoriteButton.textContent = `Add to Favorites`
-                console.log(breweryObj)
-
-            }
-        })
 
 
-    }    
+
+
+}    
 
 //Function makes the phone number easier to read
 const phoneNumberFormat = (brewery) => {
@@ -156,14 +175,14 @@ const phoneNumberFormat = (brewery) => {
         let phoneNumber = `${brewery.phone.slice(0,3)}-${brewery.phone.slice(3,6)}-${brewery.phone.slice(6,10)}`
         return phoneNumber
         //Displays phone# as-is if outside US
-        } else if ((brewery.phone !== null) && (brewery.country !== "Unite States")) {
+    } else if ((brewery.phone !== null) && (brewery.country !== "Unite States")) {
         let phoneNumber = brewery.phone
         return phoneNumber
-            //Returns "Not Available" if no phone# provided
-            } else {
-            let phoneNumber = 'Not Available'
-            return phoneNumber
-            }
+        //Returns "Not Available" if no phone# provided
+    } else {
+        let phoneNumber = 'Not Available'
+        return phoneNumber
+    }
 }
 
 //Function to clear the displayed brewery details
@@ -177,6 +196,12 @@ const resetDisplayAll =() => {
     if (document.getElementById('visited-list')) {
         document.getElementById('visited-list').remove()
     }
+    if (document.getElementById('search-form')) {
+        document.getElementById('search-form').remove()
+    }
+    if (document.getElementById('results-container')) {
+        document.getElementById('results-container').remove()
+    }
 }
 
 //Function to clear details but keep favorite/visited list displayed
@@ -186,8 +211,25 @@ const resetDisplay =() => {
     }
 }
 
+//POST to local db.json
+const addBreweryToLocal = (url, body) => {
+    const configurationObj = getConfig("POST", body)
+    return fetch(url, configurationObj)
+}
 
-//Re-usable config set up
+//PATCH to local db.json
+const updateLocalBrewery = (url, body) => {
+    const configurationObj = getConfig("PATCH", body)
+    return fetch(url, configurationObj)
+}
+
+//DELETE from local db.json
+const removeLocalBrewery = (url) => {
+    const configurationObj= getDeleteConfig(url)
+    return fetch(url, configurationObj)
+}
+
+//Re-usable POST/PATCHconfig set up
 const getConfig = (verb, body) => {
     const configurationObj = {
         method: verb,
@@ -200,9 +242,14 @@ const getConfig = (verb, body) => {
     return configurationObj
 }
 
-//POST to local db.json
-const addBreweryToLocal = (url, body) => {
-    const configurationObj = getConfig("POST", body)
+//Re-usable DELETE config set up
+const getDeleteConfig = (url) => {
+    const configurationObj = {
+        method: `DELETE`,
+        headers: {
+            "Accept": "application/json",
+        },
+    }
     return fetch(url, configurationObj)
 }
 
@@ -219,6 +266,8 @@ const filterBreweryObjKeys = (brewery) => {
         country: brewery.country,
         phone: brewery.phone,
         website: brewery.website_url,
+        visited: null,
+        favorite: null,
         notes: [],
     }
     return localBreweryObj
@@ -231,13 +280,11 @@ const showFavoritesList = () => {
     mainList.id = `favorites-list`
     mainContainer.appendChild(mainList)
     getFavorites(localUrl)
-
-
 }
 
 //GET breweries from local db
-const getFavorites = (localDB) => {
-    fetch(localDB)
+const getFavorites = () => {
+    fetch(localFavoriteUrl)
     .then(res => res.json())
     .then(favoriteBreweries => renderFavorites(favoriteBreweries))
 }
@@ -263,14 +310,14 @@ const displayFavorites = (breweryObj) => {
 const showVisitedList = () => {
     resetDisplay()
     const mainList = document.createElement('ul')
-    mainList.id = `favorites-list`
+    mainList.id = `visited-list`
     mainContainer.appendChild(mainList)
     getVisited(localUrl)
 }
 
 //GET breweries from local db
-const getVisited = (localDB) => {
-    fetch(localDB)
+const getVisited = () => {
+    fetch(localVisitedUrl)
     .then(res => res.json())
     .then(visitedBreweries => renderVisited(visitedBreweries))
 }
@@ -293,6 +340,105 @@ const displayVisited = (breweryObj) => {
 }
 
 
-const showSearchPage = () => {
-    resetDisplay()
+const checkFavoriteStatus = (breweryObj) => {
+    if(breweryObj.favorite == true){
+        return `Remove from Favorites`
+    } else {
+        return `Add to Favorites`
+    }
 }
+
+const checkVisitedStatus = (breweryObj) => {
+    if(breweryObj.visited == true){
+        return `Remove from Visited Breweries`
+    } else {
+        return `Add to Visited Breweries`
+    }
+}
+
+
+const showSearchPage = () => {
+    resetDisplayAll()
+    const searchForm = document.createElement('form')
+    const searchLabel = document.createElement('label')
+    const searchInput = document.createElement('input')
+    const searchButton = document.createElement('button')
+    const searchOption = document.createElement('select')
+    const searchByState = document.createElement('option')
+    const searchByCity = document.createElement('option')
+    const searchByName = document.createElement('option')
+    const searchByType = document.createElement('option')
+ 
+
+
+    searchForm.id = 'search-form'
+    searchLabel.textContent = 'Search Breweries By :'
+    searchInput.type = 'text'
+    searchInput.name = 'search'
+    searchButton.textContent = 'Search'
+    searchByState.value = 'state'
+    searchByState.textContent = 'State'
+    searchByCity.value = 'city'
+    searchByCity.textContent = 'City'
+    searchByName.value = 'name'
+    searchByName.textContent = 'Name'
+    searchByType.value = 'type'
+    searchByType.textContent = 'Type'
+    
+
+
+    mainContainer.appendChild(searchForm)
+    searchForm.appendChild(searchLabel)    
+    searchForm.appendChild(searchOption)
+    searchOption.appendChild(searchByCity)
+    searchOption.appendChild(searchByState)
+    searchOption.appendChild(searchByName)
+    searchOption.appendChild(searchByType)
+    searchForm.appendChild(searchInput)    
+    searchForm.appendChild(searchButton)
+       
+
+
+    searchButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        const optionValue = searchOption.value
+        const searchQuery = searchInput.value
+        const resultsContainer = document.createElement('ol')
+        resultsContainer.id = 'results-container'
+        mainContainer.appendChild(resultsContainer) 
+        clearResultsList()
+        getSearchResults(optionValue, searchQuery)
+        searchForm.reset()
+    })
+}
+
+const getSearchResults = (optionValue, searchQuery) =>{
+    fetch(`https://api.openbrewerydb.org/breweries?by_${optionValue}=${searchQuery}&per_page=50`)
+    .then(res => res.json())
+    .then(searchResults => {
+        console.log(`https://api.openbrewerydb.org/breweries?by_${optionValue}=${searchQuery}&per_page=50`)
+        resultHandler(searchResults)})
+}
+
+const resultHandler = (searchResults) => {
+    searchResults.forEach(result => displaySearchResults(result))
+}
+
+const displaySearchResults = (searchResult) => {
+    const resultContainer = document.getElementById('results-container')
+    const displayResult = document.createElement('li')
+    
+    displayResult.textContent = searchResult.name
+
+    resultContainer.appendChild(displayResult)
+
+
+}
+
+const clearResultsList = () => {
+    const resultsContainer = document.getElementById('results-container')
+    while(resultsContainer.firstChild){
+        resultsContainer.removeChild(resultsContainer.lastChild)
+    }
+}
+
